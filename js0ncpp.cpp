7fc5fea52a03750fce4b5d7d9eccb519f7763816
janezz55 <janezz55@gmail.com>
2020-09-21 22:55:16 +0200

    some fixes

---
+++ js0ncpp.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+
+#include <iostream>
+
+#include <vector>
+
+#include "js0ncpp.hpp"
+
+int main()
+{
+  jcpp::js0n const j("{\"foo\":\"bar\",\"barbar\":[1,2,3],\"obj\":[{\"a\":\"b\"}], \"a\":true}");
+
+  //
+  jcpp::dec::map("foo", std::cout, "barbar", std::cout, "obj", std::cout)(j);
+  std::cout << std::endl;
+
+  //
+  jcpp::dec::map("barbar",
+    jcpp::dec::array(
+      [](jcpp::js0n const& j){int i; jcpp::dec::decode(j, i); std::cout << i << std::endl;}
+    )
+  )(j);
+
+  //
+  struct S
+  {
+    std::string_view a;
+
+    auto decode()
+    {
+      return jcpp::dec::map("a", a);
+    }
+  } s;
+
+  //
+  jcpp::dec::map("obj", jcpp::dec::array(s))(j);
+  std::cout << s.a << std::endl;
+
+  //
+  std::vector<S> vs;
+  jcpp::dec::map("obj", vs)(j);
+  std::cout << vs.size() << " " << vs.front().a << std::endl;
+
+  return 0;
+}
---
+++ js0ncpp.hpp
@@ -0,0 +1,492 @@
+#ifndef JS0NCPP_HPP
+# define JS0NCPP_HPP
+# pragma once
+
+#include <cstring>
+
+#include <ostream>
+
+#include <sstream>
+
+#include <string_view>
+
+#include <type_traits>
+
+#include <utility>
+
+#include "js0n/src/js0n.h"
+
+namespace jcpp
+{
+
+template <typename A, typename ...B>
+struct front
+{
+  using type = A;
+};
+
+template <typename ...A>
+using front_t = typename front<A...>::type;
+
+//
+template <typename U>
+using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<U>>;
+
+//
+template <typename, typename = std::void_t<>>
+constexpr bool is_complete_v = false;
+
+template <typename T>
+constexpr bool is_complete_v<T, std::void_t<decltype(sizeof(T))>> = true;
+
+//
+template <typename T, typename = std::void_t<>>
+constexpr bool is_container_v = false;
+
+template <typename T>
+constexpr bool is_container_v<T, std::void_t<
+    std::enable_if_t<is_complete_v<typename T::const_iterator>>,
+    std::enable_if_t<is_complete_v<typename T::iterator>>,
+    std::enable_if_t<is_complete_v<typename T::size_type>>,
+    std::enable_if_t<is_complete_v<typename T::value_type>>,
+
+    decltype(std::declval<T>().cbegin()),
+    decltype(std::declval<T>().cend()),
+    decltype(std::declval<T>().begin()),
+    decltype(std::declval<T>().end()),
+    decltype(std::declval<T>().size())
+  >
+> = true;
+
+template <typename T, typename = std::void_t<>>
+constexpr bool is_associative_container_v = false;
+
+template <typename T>
+constexpr bool is_associative_container_v<T, std::void_t<
+    std::enable_if_t<is_complete_v<typename T::key_compare>>,
+    std::enable_if_t<is_complete_v<typename T::key_type>>,
+    std::enable_if_t<is_complete_v<typename T::mapped_type>>
+  >
+> = is_container_v<T>;
+
+template <typename T, typename = std::void_t<>>
+constexpr bool is_sequence_container_v = is_container_v<T>;
+
+template <typename T>
+constexpr bool is_sequence_container_v<T, std::void_t<
+    std::enable_if_t<is_complete_v<typename T::key_compare>>,
+    std::enable_if_t<is_complete_v<typename T::key_type>>,
+    std::enable_if_t<is_complete_v<typename T::mapped_type>>
+  >
+> = false;
+
+//
+template <typename T, typename = std::void_t<>>
+constexpr bool has_clear_v = false;
+
+template <typename T>
+constexpr bool has_clear_v<T, std::void_t<
+    decltype(std::declval<T>().clear())
+  >
+> = true;
+
+template <typename T, typename = std::void_t<>>
+constexpr bool has_decode_v = false;
+
+template <typename T>
+constexpr bool has_decode_v<T, std::void_t<
+    decltype(std::declval<T>().decode())
+  >
+> = true;
+
+class js0n
+{
+  std::string_view s_;
+
+  // careful! We need to move beyond s_.data(), hence we never call these
+  // functions, but calling is_array() is ok.
+  bool is_map() const noexcept
+  {
+    return is_valid() && !is_string() && ('{' == *s_.data());
+  }
+
+  bool is_string() const noexcept
+  {
+    return '"' == *(s_.data() - 1);
+  }
+
+public:
+  js0n() = default;
+
+  js0n(js0n const&) = default;
+  js0n(js0n&&) = default;
+
+  template <typename T, std::size_t N,
+    typename = std::enable_if_t<std::is_same_v<char, std::decay_t<T>>>
+  >
+  explicit js0n(T(&s)[N]) noexcept(noexcept(decltype(s_)(s, N))) :
+    s_(s, N)
+  {
+  }
+
+  template <typename ...U,
+    typename = std::enable_if_t<
+      !std::is_same_v<js0n, std::decay_t<front_t<U...>>>
+    >
+  >
+  explicit js0n(U&& ...u) noexcept(noexcept(
+    decltype(s_)(std::forward<U>(u)...))) :
+    s_(std::forward<U>(u)...)
+  {
+  }
+
+  js0n& operator=(js0n const&) = default;
+  js0n& operator=(js0n&&) = default;
+
+  template <typename U>
+  js0n& operator=(U&& u) noexcept(noexcept(s_ = std::forward<U>(u)))
+  {
+    s_ = std::forward<U>(u);
+
+    return *this;
+  }
+
+  template <typename ...U>
+  void assign(U&& ...u) noexcept(noexcept(
+    s_ = std::string_view(std::forward<U>(u)...)))
+  {
+    s_ = std::string_view(std::forward<U>(u)...);
+  }
+
+  bool is_valid() const noexcept
+  {
+    return s_.data() && s_.size();
+  }
+
+  template <typename T, std::size_t N>
+  auto operator[](T (&k)[N]) const noexcept ->
+    std::enable_if_t<std::is_same<char, std::decay_t<T>>{}, js0n>
+  {
+    std::size_t vlen;
+
+    auto const v(::js0n(k, N - 1, s_.data(), s_.size(), &vlen));
+
+    return js0n(v, std::size_t(-1) == vlen ? 0 : vlen);
+  }
+
+  auto operator[](std::string_view const& k) const noexcept
+  {
+    std::size_t vlen;
+
+    auto const v(::js0n(k.data(), k.size(), s_.data(), s_.size(), &vlen));
+
+    return js0n(v, std::size_t(-1) == vlen ? 0 : vlen);
+  }
+
+  auto operator[](std::size_t const i) const noexcept
+  {
+    std::size_t vlen;
+
+    auto const v(::js0n(nullptr, i, s_.data(), s_.size(), &vlen));
+
+    return js0n(v, std::size_t(-1) == vlen ? 0 : vlen);
+  }
+
+  bool is_array() const noexcept
+  {
+    return is_valid() && (*this)[0].is_valid();
+  }
+
+/*
+  template <typename A,
+    std::enable_if_t<
+      std::is_arithmetic<T>{} &&
+      !std::is_pointer<T>{} &&
+      !std::is_reference<T>{} &&
+      !std::is_same<T, bool>{} &&
+      !std::is_same<T, char>{} &&
+      !std::is_same<T, signed char>{} &&
+      !std::is_same<T, unsigned char>{},
+      int
+    > = 0
+  >
+  bool get(A&& a) const noexcept
+  {
+    if (is_valid())
+    {
+      auto const first(s_.data());
+      auto const last(first + s_.size());
+
+      if (T v; last == std::from_chars(first, last, r).ptr)
+      {
+        a = v
+        return false;
+      }
+    }
+
+    return true;
+  }
+*/
+
+  // size of an array, does not support objects
+  std::size_t size() const noexcept
+  {
+    std::size_t i{};
+
+    if (is_array())
+    {
+      for (;; ++i)
+      {
+        if (auto const e(operator[](i)); !e.is_valid())
+        {
+          break;
+        }
+      }
+    }
+
+    return i;
+  }
+
+  auto& view() const noexcept
+  {
+    return s_;
+  }
+};
+
+namespace dec
+{
+
+// anything can be turned into a string
+inline bool decode(js0n const& j, std::string& a) noexcept
+{
+  return j.is_valid() ? a = j.view(), false : true;
+}
+
+inline bool decode(js0n const& j, std::string_view& a) noexcept
+{
+  return j.is_valid() ? a = j.view(), false : true;
+}
+
+// bool
+template <typename A,
+  std::enable_if_t<
+    std::is_same_v<std::decay_t<A>, bool>,
+    int
+  > = 0
+>
+inline bool decode(js0n const& j, A&& a) noexcept
+{
+  if (j.is_valid())
+  {
+    if (auto const data(j.view().data()); !std::strncmp(data, "true", 4))
+    {
+      return !(a = true);
+    }
+    else if (!std::strncmp(data, "false", 5))
+    {
+      return a = false;
+    }
+  }
+
+  return true;
+}
+
+template <typename A,
+  std::enable_if_t<
+    std::is_arithmetic_v<std::decay_t<A>> &&
+    !std::is_same_v<std::decay_t<A>, bool> &&
+    !std::is_same_v<std::decay_t<A>, char> &&
+    !std::is_same_v<std::decay_t<A>, signed char> &&
+    !std::is_same_v<std::decay_t<A>, unsigned char>,
+    int
+  > = 0
+>
+inline bool decode(js0n const& j, A&& a) noexcept
+{
+  if (j.is_valid())
+  {
+    std::stringstream ss(std::string{j.view()});
+
+    if (ss >> a)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// specials
+template <typename A,
+  std::enable_if_t<
+    std::is_same_v<std::decay_t<A>, std::ostream>,
+    int
+  > = 0
+>
+inline bool decode(js0n const& j, A&& a)
+{
+  if (j.is_valid())
+  {
+    std::string_view v;
+    return decode(j, v) ? true : (a << v, false);
+  }
+  else
+  {
+    return true;
+  }
+}
+
+template <typename A,
+  std::enable_if_t<
+    std::is_invocable_r_v<bool, A, js0n const&>,
+    int
+  > = 0
+>
+inline auto decode(js0n const& j, A&& a)
+{
+  return a(j);
+}
+
+template <typename A,
+  std::enable_if_t<has_decode_v<A>, int> = 0
+>
+inline auto decode(js0n const& j, A&& a)
+{
+  return decode(j, a.decode());
+}
+
+// containers
+template <typename A,
+  std::enable_if_t<
+    is_sequence_container_v<std::decay_t<A>>,
+    int
+  > = 0
+>
+inline bool decode(js0n const& j, A&& a)
+{
+  bool error;
+
+  if (!(error = !j.is_array()))
+  {
+    if constexpr (has_clear_v<A>)
+    {
+      a.clear();
+
+      auto const sz(j.size());
+      a.reserve(sz);
+
+      for (std::size_t i{}; !error && (i != sz); ++i)
+      {
+        if (typename std::decay_t<A>::value_type v;
+          !(error = decode(j[i], v)))
+        {
+          a.push_back(std::move(v));
+        }
+      }
+    }
+  }
+
+  return error;
+}
+
+template <typename ...A>
+auto array(A&& ...a) noexcept
+{
+  return [&a...](js0n const& j)
+    {
+      bool error;
+
+      if (!(error = !j.is_array()))
+      {
+        if constexpr ((1 == sizeof...(A)) && std::is_invocable_r_v<
+          front_t<A...>, bool, std::size_t, decltype(j)>)
+        {
+          for (std::size_t i{};; ++i)
+          {
+            if (auto const e(j[i]); e.is_valid())
+            {
+              if (error = (a(i, e), ...))
+              {
+                break;
+              }
+            }
+            else
+            {
+              break;
+            }
+          }
+        }
+        else if constexpr ((1 == sizeof...(A)) &&
+          std::is_invocable_v<front_t<A...>, std::size_t, decltype(j)>)
+        {
+          for (std::size_t i{};; ++i)
+          {
+            if (auto const e(j[i]); e.is_valid())
+            {
+              (a(i, e), ...);
+            }
+            else
+            {
+              break;
+            }
+          }
+        }
+        else if constexpr ((1 == sizeof...(A)) &&
+          std::is_invocable_v<front_t<A...>, decltype(j)>)
+        {
+          for (std::size_t i{};; ++i)
+          {
+            if (auto const e(j[i]); e.is_valid())
+            {
+              (a(e), ...);
+            }
+            else
+            {
+              break;
+            }
+          }
+        }
+        else
+        {
+          if (auto const sz(j.size()); !(error = (sz != sizeof...(A))))
+          {
+            // decode the rest of the items
+            std::size_t i{};
+            error = error || (decode(j[i++], std::forward<A>(a)) || ...);
+          }
+        }
+      }
+
+      return error;
+    };
+}
+
+template <typename A, typename ...B>
+auto map(std::string_view const& key, A&& a, B&& ...b) noexcept
+{
+  static_assert(!(sizeof...(B) % 2));
+  return [key, &a, &b...](js0n const& j)
+    {
+      bool error;
+
+      if (auto const v(j[key]); !(error = !v.is_valid()))
+      {
+        error = decode(v, std::forward<A>(a));
+      }
+
+      if constexpr (bool(sizeof...(b)))
+      {
+        return error || map(std::forward<B>(b)...)(j);
+      }
+      else
+      {
+        return error;
+      }
+    };
+}
+
+}
+
+}
+
+#endif // JS0NCPP_HPP
